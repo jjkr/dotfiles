aa45922cb40b427c3e025c990994579dc32c87a8
Joe Kramer <joe.kramer.cu@gmail.com>
2014-02-10 11:59:40 -0700

    Postgres setup

--- configServer.sh
+++ configServer.sh
@@ -10,7 +10,16 @@ cp authorized_keys $HOMEDIR/.ssh
 
 # Packages
 PACKAGES="aptitude \
-          ruby \
-          postgresql"
+          ruby"
 apt-get install $PACKAGES
 
+# Postgres install
+apt-get install postgresql
+su - -c "createuser -DRSP joe" postgres
+su - -c "createdb -O joe diet" postgres
+echo "listen_addresses = '*'" >> /etc/postgresql/9.1/main/postgresql.conf
+echo "host    all             all             174.16.112.9/32            md5" \
+    >> /etc/postgresql/9.1/main/pg_hba.conf
+service postgresql restart
+
+/etc/update-motd.d/10-help-text
