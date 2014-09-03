c162fff8c17b9c6afac4d5c0e38e47c9899cea91
Joe Kramer <joe.kramer.cu@gmail.com>
2014-09-02 21:47:56 -0600

    Change awesome window border to 1px

--- _config/awesome/rc.lua
+++ _config/awesome/rc.lua
@@ -399,7 +399,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 1,
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
